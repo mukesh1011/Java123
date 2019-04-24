4bbb086272c181f73268c2c923e26805c5b69dfd
Mukesh <prajapati0810@gmail.com>
2019-04-24 21:56:46 +0100

    commit 25042019

---
+++ ArrayListChallenge/src/com/company/Contact.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+
+public class Contact {
+    private String name;
+    private String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public static Contact createContact(String name, String phoneNumber){
+        return new Contact(name, phoneNumber);
+    }
+}
